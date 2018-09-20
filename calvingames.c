57bfd07e22c3903c82ebd74e13b45578c4471507
Neptune998 <gopesh.16bcs1182@abes.ac.in>
2018-09-20 09:55:45 +0530

    this is a final

---
+++ calvingames.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int n=5,k=2,i,dp[100],a[100];
+void main()
+{
+    for (i=0;i<n;i++)
+    {
+
+        dp[i]=0;
+    }
+    for(i=0;i<n;i++)
+    {
+
+      scanf("%d",&a[i]);
+
+    }
+
+}
